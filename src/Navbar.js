35381ed8f238f2c653c409e4bbca9ef37f408f5a
Cedric-Hurst <cjhurstftw@yahoo.com>
2023-02-22 13:48:14 -0500

    Add Select And Material UI

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react'
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './Navbar.css'
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 export default class Navbar extends Component { 
+    constructor(props) {
+        super(props);
+        this.state = {colorFormat: 'hex'};
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(evt) {
+        this.setState({ colorFormat: evt.target.value });
+        this.props.handleChange(evt.target.value);
+    }
     render() {
-        const {level, changeLevel} = this.props
+        const { level, changeLevel } = this.props
+        const { colorFormat } = this.state;
         return (
             <header className='Navbar'>
                 <div className="logo">
@@ -34,7 +49,25 @@ export default class Navbar extends Component {
                             }}
                         />
                         </div>
-                    </div>
+                </div>
+                <div className='select-container'>
+                    <Box sx={{ minWidth: 100 }}>
+                        <FormControl fullWidth>
+                            <InputLabel id="select">Color Format</InputLabel>
+                            <Select
+                                labelId="select"
+                                id="color-select"
+                                value={colorFormat}
+                                label="Color Format"
+                                onChange={this.handleChange}
+                            >
+                                <MenuItem value="hex">HEX - #ffffff</MenuItem>
+                                <MenuItem value="rgb">RGB - rgb(255, 255, 255)</MenuItem>
+                                <MenuItem value="rgba">RGBA - rgba(255, 255, 255, 1.0)</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Box>
+                </div>
             </header>
         )
     }
--- src/Palette.js
+++ src/Palette.js
@@ -7,21 +7,31 @@ export default class Palette extends Component {
     constructor(props) { 
         super(props);
         this.state = {
-            level: 500
+            level: 500,
+            colorFormat: 'hex'
         }
         this.changeLevel = this.changeLevel.bind(this);
+        this.changeFormat = this.changeFormat.bind(this);
     }
     changeLevel(level) { 
         this.setState({ level });
     }
+    changeFormat(val) {
+        this.setState({colorFormat: val});
+        
+    }
     render() { 
-        const { level } = this.state;
+        const { level, colorFormat } = this.state;
         const { colors } = this.props.palette;
         const colorBoxes = colors[level].map(color =>
-            <ColorBox color={color.hex} name={color.name} />)
+            <ColorBox color={color[colorFormat]} name={color.name} />)
         return (
             <div className='Palette'>
-                <Navbar level={level} changeLevel={this.changeLevel} />
+                <Navbar
+                    level={level}
+                    changeLevel={this.changeLevel}
+                    handleChange={this.changeFormat}
+                />
                 <div className='Palette-colors'>
                     {colorBoxes}
                 </div>
