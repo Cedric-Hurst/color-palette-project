8be2d2f6d7c36a0f1a2b085e5f75d702f0dd00cf
Cedric-Hurst <cjhurstftw@yahoo.com>
2023-02-23 12:05:26 -0500

    Fix Palette Routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Palette from './Palette'
 import seedColors from './seedColors'
 import { generatePalette } from './colorHelpers'
-import {Routes, Route } from 'react-router-dom'
+import {Routes, Route, useParams } from 'react-router-dom'
+
+const findPalette = id => seedColors.find(palette => palette.id === id);
+  const GetPalette = () => {
+    const { id } = useParams();
+    const palette = generatePalette(findPalette(id))
+    return <Palette palette={palette} />
+  }
 
 function App() {
   return (
-    
     <Routes>
       <Route path='/' element={<h1>home Page</h1>}/>
-      <Route path='/Palette/:id' element={<Palette palette={generatePalette(seedColors[4])} /> } />
+      <Route path='/palette/:id' element={<GetPalette />} />
+      <Route path='*' element={<h1>Page Not Found</h1>} />
     </Routes>
   );
 }
